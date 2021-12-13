fe9b61421a0f53cb40ad8db3535bbde2ef54e9be
Claire <claire.pp@kakaocorp.com>
2021-12-13 13:44:05 +0900

    기능 구현 - 연락처 목록, 검색, 종료 나이, 번호 검증

--- StartAssignment/main.swift
+++ StartAssignment/main.swift
@@ -1,29 +1,24 @@
-//
-//  main.swift
-//  StartAssignment
-//
-//  Created by kakao on 2021/12/10.
-//
-
 import Foundation
+
+// readline에 왜 백스페이스 들어가지..?
 
 protocol InputManager {
     static func getContatct()->Contact?
-    static func getMenuInput()->String
-    static func removeWhiteSpaces(input: String)->String
+    static func getMenuInput()->String?
+    static func removeWhiteSpaces(_ input: String)->String
 }
 
 protocol ValidityChecker {
     static func isNameCorrect(name: String) -> String?
     static func isAgeCorrect(age: String) -> String?
     static func isNumberCorrect(number: String) -> String?
-    static func isMenuNumberCorrect(menuNumber: Int) -> Int?
+    static func isMenuNumberCorrect(menuNumber: String) -> String?
 }
 
 protocol MenuManager {
     var contacts: [Contact] {get set}
-    mutating func printMenu()
-    mutating func addContact()
+    func printMenu()
+    func addContact()
     func printAllContacts()
     func searchContactWithName()
 }
@@ -39,17 +34,21 @@ class Contact {
     }
 }
 
-struct InputChecker: ValidityChecker {
+class InputChecker: ValidityChecker {
 //      static func getValidValue<T>(_ checkFunction: (T) -> T?, _ target: T)-> T {
 //          if let result = checkFunction(target) {
 //              return result
 //          }
 //      }
+    // 이름이 getCorrectName이 되는 것이 맞지 않을까...
     static func isNameCorrect(name: String) -> String? {
+        if name.contains(" ") {
+            
+        }
         return name
     }
     static func isAgeCorrect(age: String) -> String? {
-        var intAge: Int = Int(age) ?? -1
+        let intAge: Int = Int(age) ?? -1
         switch intAge{
         case 0...130:
             return age
@@ -58,11 +57,15 @@ struct InputChecker: ValidityChecker {
         }
     }
     static func isNumberCorrect(number: String) -> String? {
+        let bars = number.filter { ($0) == "-" }.count
+        if bars != 2 {
+            return nil
+        }
         return number
     }
-    static func isMenuNumberCorrect(menuNumber: Int) -> Int? {
+    static func isMenuNumberCorrect(menuNumber: String) -> String? {
         switch menuNumber{
-        case 1...4:
+        case "1", "2", "3", "x":
             return menuNumber
         default:
             return nil
@@ -70,21 +73,18 @@ struct InputChecker: ValidityChecker {
     }
 }
 
-struct ContactInputManager: InputManager {
+class ContactInputManager: InputManager {
     static func getContatct()->Contact? {
         var result: Contact? = nil
         print("연락처 정보를 입력해주세요 : ", terminator:"")
-        var userInformation: String? = readLine()
+        let userInformation: String? = readLine()
         if let information: String = userInformation {
-            var processedInformation: [String] = information.split(separator: "/").map({ (subString: Substring) -> String in
-                return String(subString)
+            let processedInformation: [String] = information.split(separator: "/").map({ (subString: Substring) -> String in
+                return removeWhiteSpaces(String(subString))
             })
-//              var name = InputChecker.getValidValue(InputChecker.isNameCorrect, processedInformation[0])
-//              var age = InputChecker.getValidValue(InputChecker.isAgeCorrect, processedInformation[1])
-//              var number = InputChecker.getValidValue(InputChecker.isNumberCorrect, processedInformation[2])
-            var name = InputChecker.isNameCorrect(name: processedInformation[0])
-            var age = InputChecker.isAgeCorrect(age: processedInformation[1])
-            var number = InputChecker.isNumberCorrect(number: processedInformation[2])
+            let name = InputChecker.isNameCorrect(name: processedInformation[0])
+            let age = InputChecker.isAgeCorrect(age: processedInformation[1])
+            let number = InputChecker.isNumberCorrect(number: processedInformation[2])
             if name != nil && age != nil && number != nil {
                 // *** swift-ic??
                 result = Contact(name: name!, age: age!, number: number!)
@@ -103,25 +103,24 @@ struct ContactInputManager: InputManager {
         return result
     }
     
-    static func getMenuInput() -> String {
-        // code
-        let name: String = " asbd"
-        print(name.trimmingCharacters(in: .whitespaces))
-        return "1"
+    static func getMenuInput() -> String? {
+        let userInput = readLine()
+        return userInput ?? nil
     }
     
-    static func removeWhiteSpaces(input: String) -> String {
-        return input.trimmingCharacters(in: .whitespacesAndNewlines)
+    static func removeWhiteSpaces(_ input: String) -> String {
+        return input.replacingOccurrences(of:" ", with:"")
     }
 }
 
-struct ContactMenuManager: MenuManager {
+class ContactMenuManager: MenuManager {
+    var isEnd = false
     var contacts: [Contact] = []
-    mutating func printMenu() {
+    func printMenu() {
         print("1) 연락처 추가 2) 연락처 목록보기 3) 연락처 검색 x) 종료\n메뉴를 선택해주세요 : ", terminator:"")
-        var userInput: String? = readLine()
+        let userInput: String? = ContactInputManager.getMenuInput()
         if let userMenuInput = userInput  {
-            let menuIndex = InputChecker.isNumberCorrect(number: userMenuInput)
+            let menuIndex = InputChecker.isMenuNumberCorrect(menuNumber: userMenuInput)
             switch menuIndex {
             case "1":
                 addContact()
@@ -130,19 +129,23 @@ struct ContactMenuManager: MenuManager {
             case "3":
                 searchContactWithName()
             case "x":
-                return
+                isEnd = true
             default:
                 print("선택이 잘못되었습니다 확인 후 다시 입력해주세요")
             }
         }
     }
-    mutating func addContact() {
+    func addContact() {
         if let contact: Contact = ContactInputManager.getContatct() {
             contacts.append(contact)
         }
+        contacts = contacts.sorted(by: {$0.name < $1.name })
     }
     
     func printAllContacts() {
+        if contacts.count == 0 {
+            print("연락처가 없습니다.")
+        }
         for contact:Contact in contacts {
             print("- \(contact.name) / \(contact.age) / \(contact.number)")
         }
@@ -162,4 +165,9 @@ struct ContactMenuManager: MenuManager {
 }
 
 var menuManger: ContactMenuManager = ContactMenuManager()
-menuManger.printMenu()
+while true {
+    menuManger.printMenu()
+    if menuManger.isEnd {
+        break
+    }
+}
